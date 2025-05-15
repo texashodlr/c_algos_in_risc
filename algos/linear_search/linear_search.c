579da39e2d6e62b0c612e53ef73c39a725120f1f
texashodlr <conorxdevlin@gmail.com>
2025-05-14 21:52:15 -0600

    Linear Search
    C and Asm code!

--- algos/linear_search/linear_search.c
+++ algos/linear_search/linear_search.c
@@ -1,1 +1,30 @@
 //This is the C implementation of Linear Search
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    srand((unsigned int)time(NULL));
+
+    int arr[50];
+    
+    for (int i = 0; i<50; i++){
+        arr[i] = (rand()%99)+1;  //Integer between 1-99
+        printf("Index: %d | Value: %d \n",i, arr[i]);
+    }
+
+    int search = (rand()%99)+1;
+    printf("Searching for..... %d\n",search);
+    for(int i = 0; i<50; i++){
+        if(search == arr[i]){
+            printf("%d found at Index: %d..... exiting\n",search,i);
+            return 0;
+        }
+    }
+
+    printf("%d not found.... exiting\n",search);
+
+
+    return 0;
+}
---
+++ algos/linear_search/linear_search_as.asm
@@ -0,0 +1,72 @@
+# Linear Seach in assembly
+# Implementation of a simplified version of my linear search
+# int arr[5] = {14, 67, 23, 89, 45};  // fixed values
+# int search = 23;
+
+# for (int i = 0; i < 5; i++) {
+# /  if (arr[i] == search) {
+#        // found at i
+#        break;
+#    }
+# }
+
+.data
+arr: .word 14, 67, 23, 89, 45       # Array of integers
+search: .word 23                    # What we're searching for
+found: .string "Found at index: "
+notfound: .string "Not found at index\n"
+newline: .string "\n"
+
+# Code Section
+.text
+
+# Main Entry Point
+# Program Starts here at 0x00000000
+
+main: 
+    # initializations
+    li t0, 0        # index i = 0
+    la t1, arr      # address of arr[0]
+    lw t3, search   # load search value
+    li t4, 5        # Max iterations
+loop:
+    beq t0, t4, not_found    # if i == 5, done
+
+    slli t5, t0, 2          # offset = i*4
+    add t6, t1, t5          # addr = base + offset
+    lw t2, 0(t6)            # load arr[i]
+
+    beq t2, t3, _found       # if arr[i] == search
+
+    addi t0, t0, 1          # i++
+    j loop
+
+_found:
+    # print "Found at index" - Env call 11
+    la a0, found
+    li a7, 4
+    ecall
+    
+    # Load index 
+    mv a0, t0
+    li a7, 1
+    ecall
+
+    # Print newline
+    la a0, newline
+    li a7, 4
+    ecall
+
+    j end
+
+not_found:
+    # print "Not found at index" - Env call 11
+    la a0, notfound
+    li a7, 4
+    ecall
+    j end
+
+end:
+    # Exit
+    li a7, 10
+    ecall
