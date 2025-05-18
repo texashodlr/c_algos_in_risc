6a064688e1849c92fb6a93333bc73d190615db70
texashodlr <conorxdevlin@gmail.com>
2025-05-17 20:10:48 -0600

    Completed binary_search in C

--- algos/binary_search/binary_search.c
+++ algos/binary_search/binary_search.c
@@ -3,11 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 
 //Binary Search function, rx's a sorted array and search value, then returns T/F if the
 // search value is present in the array
-//void binary_search(){}
+int binary_search(int search, int *arr, int size){
+
+    if (search > arr[size-1]){ return -1;}
+    else{
+        int low_bound = 0;
+        int high_bound = size-1;
+        while (low_bound <= high_bound){
+            int middle = (low_bound + high_bound) /2;
+            if(search == arr[middle])
+                return middle;
+            else if (search < arr[middle]){
+                high_bound = middle -1;
+            }
+            else{
+                low_bound = middle + 1;
+            }
+        }
+    }
+    return -1;
+}
 
 
 // [1,2,3,4,2]
@@ -38,7 +58,7 @@ void generate_array(int *arr, int size){
             num_ones++;
         }
     }
-    printf("Number of ones: %d",num_ones);
+    //printf("Number of ones: %d",num_ones);
 }
 
 int main(){
@@ -47,12 +67,22 @@ int main(){
     srand((unsigned int)time(NULL));
     generate_array(arr, size);
     sort(arr, size);
-    
+    /*
     for (int i = 0; i<size; i++){
         printf("Index: %d | Value: %d\n",i, arr[i]);
+    }*/
+
+    int search = (rand()%99999)+1;
+    int found_index = 0;
+    int count = 0;
+    while((found_index == 0 || found_index == -1) && (count < 100)){
+        found_index = binary_search(search, arr, size);
+        count++;
+        search--;
     }
 
-    //int search = (rand()%99)+1;
+    
+    printf("Searching for: %d Found Index: %d",search, found_index);
 
     return 0;
 }
